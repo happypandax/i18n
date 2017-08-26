80d2bd0bcc0c5c4d48701e70127ff5335946fe50
Twiddly <pew@pewpew.moe>
2017-08-26 04:08:30 +0200

    improved yaml loader;

--- i18n/loaders/yaml_loader.py
+++ i18n/loaders/yaml_loader.py
@@ -6,9 +6,17 @@ class YamlLoader(Loader):
     """class to load yaml files"""
     def __init__(self):
         super(YamlLoader, self).__init__()
-
+        
     def parse_file(self, file_content):
         try:
-            return yaml.load(file_content)
+            return yaml.safe_load(file_content)
         except yaml.scanner.ScannerError as e:
-            raise I18nFileLoadError("invalid YAML: {0}".format(str(e)))
+            raise i18n.loaders.loader.I18nFileLoadError("invalid YAML: {0}".format(str(e)))
+
+    def get_data(self, data, root_data):
+        return data
+    
+    def load_resource(self, filename, root_data):
+        file_content = self.load_file(filename)
+        data = self.parse_file(file_content)
+        return self.get_data(data, root_data)
